85a78cc8c613943d1e82e12c9197e986b215c53c
Luka Panio <lukapanio@gmail.com>
2025-01-03 15:52:37 +0100

    q5q: Update blobs from F946BXXS4DXKC
    Change-Id: Ic044f353275591820ba296d65883ad167121b55b

--- lineage_q5q.mk
+++ lineage_q5q.mk
@@ -7,6 +7,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
 # Inherit from the device configuration.
 $(call inherit-product, device/samsung/q5q/device.mk)
@@ -23,6 +24,5 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="q5qxxx-user 14 UP1A.231005.007 F946BXXS1BWKI release-keys"
-
-BUILD_FINGERPRINT := samsung/q5qxxx/q5q:13/TP1A.220624.014/F946BXXS1BWKI:user/release-keys
+    BuildDesc="q5qxxx-user 14 UP1A.231005.007 F946BXXS4DXKC release-keys" \
+    BuildFingerprint=amsung/q5qxxx/q5q:13/TP1A.220624.014/F946BXXS4DXKC:user/release-keys
